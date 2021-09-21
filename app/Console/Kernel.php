49102e1df976345f1dbb19360668ef630ce5cc14
habsyi-ikhlashulariq <habsyi.ikhlashulariq@gmail.com>
2021-09-21 21:26:45 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,6 +26,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        // Masukkan Kode Anda Disini
+        $schedule->call(function () {
+            
+            //Pengecekan apakah cronjob berhasil atau tidak
+        //Mencatat info log 
+                Log::info('Cronjob berhasil dijalankan');
+            })->everyTwoMinutes();
     }
 
     /**
